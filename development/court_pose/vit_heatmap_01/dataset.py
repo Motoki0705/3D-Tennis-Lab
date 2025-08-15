509d3bb2f1499df95fc84263286437d434e6fcf3
Motoki0705 <kamimura098@gmail.com>
2025-08-15 11:27:29 +0000

    fix: pathの扱いを変更（to_absolute_pathを使用）

--- development/court_pose/vit_heatmap_01/dataset.py
+++ development/court_pose/vit_heatmap_01/dataset.py
@@ -7,6 +7,7 @@ import cv2
 import numpy as np
 import torch
 from albumentations.pytorch import ToTensorV2
+from hydra.utils import to_absolute_path
 from torch.utils.data import Dataset
 
 
@@ -45,8 +46,12 @@ class CourtKeypointDataset(Dataset):
         image_id = annotation["image_id"]
         image_meta = self.image_info[image_id]
 
-        img_path = self.img_dir / image_meta["file_name"]
-        image = cv2.imread(str(img_path))
+        rel_path = self.img_dir / image_meta["file_name"]
+        img_path = to_absolute_path(str(rel_path))
+        image = cv2.imread(img_path)
+        if image is None:
+            raise FileNotFoundError
+
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         original_h, original_w, _ = image.shape
